1b85abfaa3530ac3a8a339173e333e7acf82691c
François Simond <dev@supercurio.net>
2026-04-18 15:21:19 +0200

    Add Gradle tasks to run examples
    As well as a cleanup task to delete example FIT files generated

--- fit-sdk/build.gradle.kts
+++ fit-sdk/build.gradle.kts
@@ -59,3 +59,51 @@ val dokkaJavadocJar by tasks.registering(Jar::class) {
     from(tasks.dokkaGeneratePublicationJavadoc.flatMap { it.outputDirectory })
     archiveClassifier.set("javadoc")
 }
+
+tasks.register<JavaExec>("runDecodeExample") {
+    registerExampleCli(desc = "FIT Decode Example Application", className = "DecodeExample")
+}
+
+tasks.register<JavaExec>("runDecoderExample") {
+    this.registerExampleCli(desc = "FIT Decoder Example Application", className = "DecoderExample")
+}
+
+tasks.register<JavaExec>("runEncodeActivityExample") {
+    this.registerExampleCli(desc = "FIT Encode Activity Example", className = "EncodeActivity")
+}
+
+tasks.register<JavaExec>("runEncodeCourseExample") {
+    this.registerExampleCli(desc = "FIT Encode Course Example", className = "EncodeCourse")
+}
+
+tasks.register<JavaExec>("runEncodeExample") {
+    this.registerExampleCli(desc = "FIT Encode Example", className = "EncodeExample")
+}
+
+tasks.register<JavaExec>("runEncodeWorkoutExample") {
+    this.registerExampleCli(desc = "FIT Encode Workout Example", className = "EncodeWorkout")
+}
+
+tasks.register<JavaExec>("runFitDecoderExample") {
+    this.registerExampleCli(desc = "FIT Decoder Example", className = "EncodeWorkout")
+}
+
+private fun JavaExec.registerExampleCli(desc: String, className: String) {
+    group = "application"
+    description = "Runs $desc"
+    mainClass.set("com.garmin.fit.examples.$className")
+    classpath = sourceSets["jvmMain"].runtimeClasspath
+
+    if (project.hasProperty("args")) {
+        args = project.property("args").toString().split(" ")
+    }
+}
+
+tasks.register<Delete>("cleanFitFiles") {
+    group = "cleanup"
+    description = "Deletes all .fit files in the project directory"
+
+    delete(fileTree(projectDir) {
+        include("**/*.fit")
+    })
+}
